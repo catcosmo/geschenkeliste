9aa8159405253c8a6dbf7976cfb8ebd9cf46d8be
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-12-02 17:14:48 +0100

    Added scripts for bmf 2015 and 2016

---
+++ scripts/002-bmf2015.py
@@ -0,0 +1,58 @@
+# coding=utf-8
+
+import codecs
+import csv
+
+with open('../data/csvTxtPreprocess/bmf2015.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+
+    result = []
+
+    for index, row in enumerate(reader):
+        print(index, row)
+
+        value = row[0].split(' €', 1)[0]
+        value = value.replace(',', '.')
+        if value.startswith('ca'):
+            print('does', value)
+            value = value.split(' ', 1)[1]
+
+        if value.startswith('<'):
+            value = None
+
+        day, month, year = None, None, None
+        gift = row[1].strip()
+
+        under25euro = False
+        try:
+            under25euro = float(value) < 25
+        except Exception as e:
+            under25euro = (row[0][0] == '<')
+            pass
+
+        over25euro = False
+        try:
+            over25euro = float(value) > 25
+        except Exception as e:
+            pass
+
+        fate = row[3] if len(row) >= 3 else None
+        success = 'ja' in row[2]
+        ministryabbreviation = 'bmf'
+
+        result.append([
+            gift,
+            day,
+            month,
+            year,
+            value,
+            under25euro,
+            over25euro,
+            fate,
+            success,
+            ministryabbreviation
+        ])
+
+    with open('bmf-2015.csv', 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerows(result)
