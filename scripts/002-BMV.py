c3229777ace49abf3c45616cfa76a0650001c92d
catcosmo <astrangethought@hotmail.com>
2016-12-04 22:15:11 +0100

    bmv final

--- scripts/002-BMV.py
+++ scripts/002-BMV.py
@@ -2,12 +2,13 @@
 import csv
 import re
 
-newRow = ['gift', 'Bundesministerium der Verteidigung','seit 10/2009', 'n.a.', 'n.a.', 'BMV', 'fail']
+oldRow = ['gift', 'Bundesministerium der Verteidigung','seit 10/2009', 'n.a.', 'n.a.', 'BMV', 'fail']
+newRow = ['gift', '0000', '00', '00', '00.00', 'false', 'false', 'fate', 'n.a.', 'BMI']
 sheet =  open('bmv.csv', 'w')
 
 
 
-with open('data/csvTxtPreprocess/bmvPre.csv', 'r+') as file:
+with open('../data/csvTxtPreprocess/bmvPre.csv', 'r+') as file:
     data = csv.reader(file, delimiter=',')
 
     for row in data:
@@ -16,11 +17,28 @@ with open('data/csvTxtPreprocess/bmvPre.csv', 'r+') as file:
         # get gift
         newRow[0] = row[0]
         # get value
-        newRow[3] = row[1]
+        if '< 25' in row[1]:
+            newRow[5] = 'True'
+            newRow[4] = '00.00'
+        else:
+            newRow[5] = 'False'
+            newRow[4] = re.sub('[A-Za-z€\s\"()$>?.]', '', row[1])
+            newRow[4] = re.sub('\,', '.', newRow[4])
+            newRow[4] = '0' + newRow[4]
+
+            # get 25er bools
+            if float(newRow[4]) > 25:
+                newRow[6] = 'True'
+                newRow[5] = "False"
+            elif float(newRow[4]) < 25:
+                newRow[5] = 'True'
+                newRow[6] = "False"
         # get fate
-        newRow[4] = row[2]
+        newRow[7] = row[2]
         # get success
         if ('Bezahlung' in row[2]) | ('kostenfreie' in row[2]):
-            newRow[6] = 'success'
+            newRow[8] = 'success'
+        else:
+            newRow[8] = 'fail'
 
         sheet.write(', '.join(newRow) + '\n')
